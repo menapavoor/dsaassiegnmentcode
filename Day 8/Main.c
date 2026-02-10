4e49df68cdcd07893eaea95b7907c1902d6a4fe0
menapavoor <menapavoor@gmail.com>
2026-02-10 23:51:49 +0530

    Add recursive power function in C

---
+++ Day 8/Main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+long long power(long long a, long long b) {
+    if (b == 0)
+        return 1;              // Base case: a^0 = 1
+    return a * power(a, b - 1); // Recursive step
+}
+
+int main() {
+    long long a, b;
+    scanf("%lld %lld", &a, &b);
+
+    printf("%lld", power(a, b));
+    return 0;
+}
