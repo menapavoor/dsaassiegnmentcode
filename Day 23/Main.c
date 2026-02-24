8edab923cfb5a31702248fdc3da1c11990ed9579
menapavoor <mena.pavoor@gmail.com>
2026-02-25 02:15:51 +0530

    Updated all days up to Day23

---
+++ Day 23/Main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert node at the end
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    if (head == NULL) {
+        return newNode;
+    }
+    struct Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    return head;
+}
+
+// Function to merge two sorted linked lists
+struct Node* mergeLists(struct Node* list1, struct Node* list2) {
+    if (list1 == NULL) return list2;
+    if (list2 == NULL) return list1;
+
+    struct Node* result = NULL;
+
+    if (list1->data <= list2->data) {
+        result = list1;
+        result->next = mergeLists(list1->next, list2);
+    } else {
+        result = list2;
+        result->next = mergeLists(list1, list2->next);
+    }
+    return result;
+}
+
+// Function to print linked list
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, m, val;
+    struct Node* list1 = NULL;
+    struct Node* list2 = NULL;
+
+    // Input first list
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        list1 = insertEnd(list1, val);
+    }
+
+    // Input second list
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &val);
+        list2 = insertEnd(list2, val);
+    }
+
+    // Merge lists
+    struct Node* mergedList = mergeLists(list1, list2);
+
+    // Print merged list
+    printList(mergedList);
+
+    return 0;
+}
