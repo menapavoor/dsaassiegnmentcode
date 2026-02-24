4bf89aa10357ea8caf241cc61bab46d631ecac71
menapavoor <mena.pavoor@gmail.com>
2026-02-25 02:08:31 +0530

    Updated all days up to Day22

---
+++ Day 21/Main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create linked list from array
+struct Node* createList(int arr[], int n) {
+    if (n == 0) return NULL;
+
+    struct Node* head = createNode(arr[0]);
+    struct Node* current = head;
+
+    for (int i = 1; i < n; i++) {
+        current->next = createNode(arr[i]);
+        current = current->next;
+    }
+
+    return head;
+}
+
+// Function to traverse and print linked list
+void traverseList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* head = createList(arr, n);
+    traverseList(head);
+
+    return 0;
+}
