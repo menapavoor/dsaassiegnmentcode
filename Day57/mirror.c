6c0b1db165399ceffb5df483a6580776764a21bb
menapavoor <mena.pavoor@gmail.com>
2026-03-30 23:26:20 +0530

    Day 57 Mirror Binary Tree

---
+++ Day57/mirror.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create node
+struct Node* createNode(int data)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Mirror tree
+void mirror(struct Node* root)
+{
+    if (root == NULL)
+        return;
+
+    // swap
+    struct Node* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+
+    mirror(root->left);
+    mirror(root->right);
+}
+
+// Inorder traversal
+void inorder(struct Node* root)
+{
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    struct Node* nodes[n];
+
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    // create nodes
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    // connect tree
+    for(int i=0;i<n;i++)
+    {
+        if(nodes[i] != NULL)
+        {
+            int l = 2*i + 1;
+            int r = 2*i + 2;
+
+            if(l < n) nodes[i]->left = nodes[l];
+            if(r < n) nodes[i]->right = nodes[r];
+        }
+    }
+
+    struct Node* root = nodes[0];
+
+    mirror(root);
+    inorder(root);
+
+    return 0;
+}
