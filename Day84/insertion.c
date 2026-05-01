4c3d3f32a632edaafcb42512369888f8892caae9
menapavoor <mena.pavoor@gmail.com>
2026-05-01 09:15:26 +0530

    Day 84 Insertion Sort implementation

---
+++ Day84/insertion.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define MAX 1000
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[MAX];
+
+    // input array
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // insertion sort
+    for(int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        // shift elements greater than key
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    // output sorted array
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
